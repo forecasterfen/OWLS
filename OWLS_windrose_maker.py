462d7ef22e0bc7052dc012f5e82a30afe580dbb8
Forecaster Fen <106565986+forecasterfen@users.noreply.github.com>
2023-03-12 22:06:21 -0400

    Update OWLS_windrose_maker.py

--- OWLS_windrose_maker.py
+++ OWLS_windrose_maker.py
@@ -1,3 +1,7 @@
+#For this program to work, start and end variables use datetime function, example is datetime(yyyy,m,d) if month and/or day has double digits
+#then you will add the second digit, otherwise, just add only one digit. The start and end defines how farback you want to grab the data and
+#how far foward you want your data from
+
 %matplotlib inline
 import pandas as pd
 import numpy as np
@@ -7,20 +11,17 @@ from meteostat import Point, Hourly
 from windrose import WindroseAxes
 from datetime import datetime
 
-#For this program to work, start and end variables use datetime function, example is datetime(yyyy,m,d) if month and/or day has double digits
-#then you will add the second digit, otherwise, just add only one digit. The start and end defines how farback you want to grab the data and
-#how far foward you want your data from
-
 def windRoseElevation(latitude, longitude, elevation, start, end):
     # Define location
     location = Point(latitude, longitude, elevation)
     # Grab data for location
     data = Hourly(location, start, end)
     df = data.fetch()
+    normalized_df=(df-df.mean())/df.std()
     #Make WindRose
     ax = WindroseAxes.from_ax()
-    ax.set_title("Wind Rose for coordinates "+str(latitude)+" , "+str(longitude)+ ' at elevation '+str(elevation) + 'm')
-    ax.bar(df.wdir, df.wspd, normed=True, opening=0.8, edgecolor='white')
+    ax.set_title("Wind Rose for coordinates "+str(latitude)+" , "+str(longitude)+ ' at elevation '+str(elevation) + 'm'+ ' from '+str(start)+' to '+str(end))
+    ax.bar(df.wdir, normalized_df.wspd, normed=True, opening=0.8, edgecolor='white')
     ax.set_legend()
     
 def windRose(latitude, longitude, start, end):
@@ -29,8 +30,9 @@ def windRose(latitude, longitude, start, end):
     # Grab data for location
     data = Hourly(location, start, end)
     df = data.fetch()
+    normalized_df=(df-df.mean())/df.std()
     #Make WindRose
     ax = WindroseAxes.from_ax()
-    ax.set_title("Wind Rose for coordinates "+str(latitude)+" , "+str(longitude))
-    ax.bar(df.wdir, df.wspd, normed=True, opening=0.8, edgecolor='white')
+    ax.set_title("Wind Rose for coordinates "+str(latitude)+" , "+str(longitude)+ ' from '+str(start)+' to '+str(end))
+    ax.bar(df.wdir, normalized_df.wspd, normed=True, opening=0.8, edgecolor='white')
     ax.set_legend()
